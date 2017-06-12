85dc27a4b5c5fe5418ec4528619b4d4fb7e76b61
dudleyinc3 <dudleyinc3@gmail.com>
2017-06-12 23:55:09 +0300

    bookmarks placing and fetching

---
+++ js/main.js
@@ -0,0 +1,68 @@
+//Listen for submit
+
+	document.getElementById('myForm').addEventListener('submit', saveBookmark); 
+	
+
+	function saveBookmark(e) {
+		var siteName =document.getElementById('siteName').value;
+		var siteUrl =document.getElementById('siteUrl').value;
+			
+		var bookmark = {
+
+			name: siteName,
+			url: siteUrl
+		}
+
+	/*local storage test*/
+	/*	localStorage.setItem('test', 'Hello World');
+		localStorage.getItem('test');
+		localStorage.removeItem('test');
+		console.log(localStorage.getItem('test'));
+	*/
+
+		 if(localStorage.getItem('bookmarks') === null){
+    // Init array
+    var bookmarks = [];
+    // Add to array
+    bookmarks.push(bookmark);
+    // Set to localStorage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }	else {
+  		var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  		//add new bookmark to array
+  		bookmarks.push(bookmark);
+  		//set to local storage
+   	localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
+
+		//prevent from submitting
+		e.preventDefault();	
+	}
+
+
+	// Fetch bookmarks
+	function fetchBookmarks(){
+		//get from local storage
+		var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+	console.log(bookmarks);
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
